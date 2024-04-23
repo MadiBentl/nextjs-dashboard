5d7827ae8ae35c4f7964329d74df50911000a847
madibentl <mb.madisonbentley@gmail.com>
2024-04-23 08:18:38 -0400

    ch 9

---
+++ app/dashboard/(overview)/loading.tsx
@@ -0,0 +1,7 @@
+import DashboardSkeleton from '@/app/ui/skeletons';
+ 
+export default function Loading() {
+  return <DashboardSkeleton />;
+}
+
+// the parentheses around 'overview' makes it so a new route isn't created.
